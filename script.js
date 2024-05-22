eec5620b55a026d282d78690897cb4e2c9448c9d
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-22 22:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let val=document.getElementById('level');
+let count=0;
+
+while (val) {
+	count++;
+	val=val.parentElement;
+}
+
+let s="The level of the element is: "+count;
+
+alert(s);
